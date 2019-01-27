9ef2262a98354674a4d53872023719731cdc00e4
DESKTOP-PCRS882\gonbe <gonbeee.project@gmail.com>
2019-01-27 14:08:06 +0900

    FirstCommit

---
+++ Assets/Vibrater.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vibrater : MonoBehaviour
+{
+
+    [SerializeField]
+    private int vibCount;
+
+    [SerializeField,Range(500,3999)]
+    private int vibPower;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var trackedObject = GetComponent<SteamVR_TrackedObject>();
+        var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            // トリガーを深く引いている
+            device.TriggerHapticPulse((ushort)vibPower);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Vib());
+        }
+    }
+
+    IEnumerator Vib()
+    {
+        var trackedObject = GetComponent<SteamVR_TrackedObject>();
+        var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        for(int i = 0; i < vibCount; i++)
+        {
+            device.TriggerHapticPulse((ushort)vibPower);
+
+            yield return null;
+        }
+
+    }
+}
